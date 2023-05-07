0658094f797cc9d34a264f66a0f7af5459843a3b
Mehdi HYANI <hyanimehdi115@gmail.com>
2023-05-07 22:05:15 +0100

    fix: sign in callback

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { type NextPage } from "next";
 import Image from 'next/image';
 import loginPageImage from "../../assets/loginIllustration.png"
@@ -20,7 +21,7 @@ const loginPage: NextPage = () => {
                 <p className="text-xl font-bold">Log in to connect with your professors and schedule your appointments.</p>
             </div>
             <div className="mt-10 flex justify-center items-center">
-                <button onClick={() => void signIn()} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">            
+                <button onClick={() => signIn('azure-ad')} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">            
                     <span className="relative px-20 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                     <div className="flex gap-3">
                     <Image className="h-6 w-6" src={outlookIcon} alt=''/>
