6670e90cad2ac3ac63181ccb6dc430da70d1522b
Mehdi HYANI <hyanimehdi115@gmail.com>
2023-05-07 18:28:03 +0100

    fix: eslint issues fix

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,7 +8,7 @@ import AzureADProvider from "next-auth/providers/azure-ad";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
-import { User, UserRole } from "@prisma/client";
+import type { User, UserRole } from "@prisma/client";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
