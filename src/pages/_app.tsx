bcfb7e963844b113cd5385b8ffd49c19cb5d9b39
Mehdi HYANI <hyanimehdi115@gmail.com>
2023-05-09 13:28:32 +0100

    feat: pages and routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import theme from "~/utils/theme";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -13,7 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
---
+++ src/utils/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+    colors: {
+        proffice: {
+            blue: "#4173AB",
+        },
+    },
+})
+
+export default theme;
