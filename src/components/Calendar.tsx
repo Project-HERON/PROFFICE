5e27309df76b73ec8dd69eaeb029c7766879dde6
Mehdi HYANI <hyanimehdi115@gmail.com>
2023-05-11 18:34:36 +0100

    feat: delete availability modal

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,22 +2,28 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Skeleton } from '@chakra-ui/react'
+import { Skeleton, useDisclosure } from '@chakra-ui/react'
 import { useToast } from '@chakra-ui/react'
 
 import { api } from '~/utils/api';
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
+import AvailabilityInfoModal from './modals/AvailabilityInfoModal'
+import { useState } from 'react'
 
 const Calendar = () => {
 
     const toast = useToast();
     const router = useRouter();
+    const {
+        isOpen: isAvailabilityInfoModalOpen, onOpen: onAvailabilityInfoModalOpen, onClose: onAvailabilityInfoModalClose
+    } = useDisclosure({ defaultIsOpen: false })
     const { data: sessionData } = useSession();
     const { isLoading: isCalendarLoading, data: myCalendar, error: calendarError } =
         api.officeHoursSession.getMyCalendar.useQuery(
             undefined, { refetchInterval: 5000 }
         );
+    const [availabilityId, setAvailabilityId] = useState<string>('')
 
     if (isCalendarLoading)
         return <Skeleton height='80vh' speed={1.2} />;
@@ -34,34 +40,40 @@ const Calendar = () => {
     }
 
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            initialView='timeGridWeek'
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={3}
-            events={
-                myCalendar.map((session) => ({
-                    id: session.id,
-                    date: session.startDate,
-                    end: session.endDate,
-                    editable: false,
-                    durationEditable: false,
-                    title: sessionData!.user.role === 'professor' ? session.officeHoursSession ? `Session with ${session.officeHoursSession.student.name?.split('<')[0] || ''}` : `Available` : `Session with Professor: ${session.professor.name?.split('<')[0] || ''}`,
-                    backgroundColor: session.officeHoursSession ? 'red' : 'green',
-                    display: ''
-                }))
-            }
-            eventOverlap={false}
-            // eventClick={}
-            // businessHours={[]}
-        />
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                }}
+                initialView='timeGridWeek'
+                editable={true}
+                selectable={true}
+                selectMirror={true}
+                dayMaxEvents={3}
+                events={
+                    myCalendar.map((session) => ({
+                        id: session.id,
+                        date: session.startDate,
+                        end: session.endDate,
+                        // editable: true,
+                        interactive: true,
+                        durationEditable: false,
+                        title: sessionData!.user.role === 'professor' ? session.officeHoursSession ? `Session with ${session.officeHoursSession.student.name?.split('<')[0] || ''}` : `Available` : `Session with Professor: ${session.professor.name?.split('<')[0] || ''}`,
+                        backgroundColor: session.officeHoursSession ? 'red' : 'green',
+                        display: ''
+                    }))
+                }
+                eventOverlap={false}
+                eventClick={(e) => {
+                    setAvailabilityId(e.event.id)
+                    onAvailabilityInfoModalOpen();
+                }}
+            />
+            {isAvailabilityInfoModalOpen && availabilityId ? <AvailabilityInfoModal availabilityId={availabilityId} onClose={onAvailabilityInfoModalClose} /> : <></>}
+        </>
     );
 };
 
---
+++ src/components/modals/AvailabilityInfoModal.tsx
@@ -0,0 +1,118 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+import {
+    Button, Modal, ModalOverlay,
+    ModalContent, ModalHeader,
+    ModalCloseButton, ModalBody,
+    FormControl, FormLabel,
+    ModalFooter,
+    Input,
+    useToast,
+    Skeleton
+} from "@chakra-ui/react";
+import { api } from "~/utils/api";
+import { TRPCClientError } from "@trpc/client";
+import moment from "moment";
+import { useRouter } from "next/router";
+
+const AvailabilityInfoModal = ({ availabilityId, onClose }: { availabilityId: string; onClose: () => void }) => {
+
+    const {
+        data: availability, isLoading: availabilityLoading, error: availabilityError
+    } = api.availability.getAvailability.useQuery({ availabilityId });
+    const {
+        mutateAsync: deleteAvailability, isLoading: deleteAvailabilityLoading
+    } = api.availability.deleteAvailability.useMutation();
+    const {
+        mutateAsync: cancelSession, isLoading: cancelSessionLoading
+    } = api.officeHoursSession.cancelOfficeHoursSession.useMutation()
+    const toast = useToast();
+    const router = useRouter();
+
+
+    if (availabilityLoading)
+        return <Skeleton height='20vh' speed={1.2} />
+
+    if (availabilityError) {
+        void router.push('/500');
+        return <></>;
+    }
+
+    const handleSubmit = async () => {
+        try {
+            if (availability.officeHoursSession)
+                await cancelSession({ officeHoursSessionId: availability.officeHoursSession.id });
+            else
+                await deleteAvailability({ availabilityId });
+
+            toast({
+                title: availability.officeHoursSession ? 'Successfully cancelled the session' : 'Successfully deleted the availability',
+                status: 'success'
+            })
+
+            onClose();
+
+        } catch (error) {
+            if (error instanceof TRPCClientError) {
+                try {
+                    const messages = JSON.parse(error.message) as Array<{ code: string | number; message: string; path: Array<string> }>;
+                    toast({
+                        title: 'Error deleting the availability',
+                        description: messages.map(m => m.message).join(' - '),
+                        status: 'error'
+                    })
+                } catch (err) {
+                    toast({
+                        title: 'Error deleting the availability',
+                        description: error.message,
+                        status: 'error'
+                    })
+                }
+            }
+
+            else
+                toast({
+                    title: 'Error deleting the availability',
+                    description: 'Something went wrong',
+                    status: 'error'
+                })
+
+        }
+    }
+
+    return (
+        <Modal
+            isOpen
+            onClose={onClose}
+            size='3xl'
+        >
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>{availability.officeHoursSession ? `Session with ${availability.officeHoursSession.student.name || ''}` : `Availability information`}</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody pb={6}>
+                    <FormControl>
+                        <FormLabel>Availability start:</FormLabel>
+                        <Input defaultValue={moment(availability.startDate).format('dddd, MMMM Do YYYY, h:mm a')} disabled placeholder='Start Date' />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Availability end:</FormLabel>
+                        <Input defaultValue={moment(availability.endDate).format('dddd, MMMM Do YYYY, h:mm a')} disabled placeholder='Start Date' />
+                    </FormControl>
+                    {availability.officeHoursSession && availability.officeHoursSession.student.name ? <FormControl mt={4}>
+                        <FormLabel>Booked by:</FormLabel>
+                        <Input disabled defaultValue={availability.officeHoursSession.student.name} placeholder='Availability booked by' />
+                    </FormControl> : <></>}
+                </ModalBody>
+                <ModalFooter>
+                    <Button isLoading={deleteAvailabilityLoading || cancelSessionLoading} disabled={!availabilityId} onClick={handleSubmit} colorScheme='red' mr={3}>
+                        {availability.officeHoursSession ? 'Cancel Session' : 'Delete Availability'}
+                    </Button>
+                    <Button onClick={onClose}>Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default AvailabilityInfoModal;
